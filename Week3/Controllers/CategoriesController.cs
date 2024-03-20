cb1c4c06f75739764ad7c0f69613e23163d1f995
KKai999 <20231739@ctucareer.co.za>
2024-03-20 12:52:16 +0200

    Add project files.

---
+++ Week3/Controllers/CategoriesController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Week3.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        } public IActionResult Edit(int ? id)
+        {
+            //return View();
+            if (id.HasValue)
+                return new ContentResult { Content = id.ToString() };
+            else
+            {
+                return new ContentResult { Content = "Not Found content" };
+            }
+        }
+    }
+}
